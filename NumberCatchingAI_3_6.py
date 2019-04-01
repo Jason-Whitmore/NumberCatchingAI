11ea7bfaf6840f462c93b406ce74486712ac31c8
Jason-Whitmore <jasonwhitmore12@gmail.com>
2019-04-01 14:42:15 -0700

    Updating files with documentation

--- NumberCatchingAI_3_6.py
+++ NumberCatchingAI_3_6.py
@@ -34,7 +34,8 @@ class NumberCatchingAI:
         self.numCols = 15
         self.numRows = 25
 
-
+        for i in range(5):
+            self.numberInfo.append(NumberData(random.randint(1,9), random.randint(0,14), i * 5))
 
         #PPO hyperparameters
         self.discountFactor = 0.95
@@ -44,9 +45,6 @@ class NumberCatchingAI:
         self.GAELambda = 0.95
         
         self.epsilon = 0.2
-
-        for i in range(5):
-            self.numberInfo.append(NumberData(random.randint(1,9), random.randint(0,14), i * 5))
 
 
         #Other hyperparameters
@@ -68,7 +66,6 @@ class NumberCatchingAI:
         self.policyFunction.add(Dense(3))
         self.policyFunction.add(Activation('softmax'))
 
-
         self.policyFunction.compile(optimizer=keras.optimizers.SGD(lr=policyLR), loss="mse")
 
 
@@ -78,7 +75,9 @@ class NumberCatchingAI:
         self.valueFunction = Sequential()
 
         self.valueFunction.add(Dense(64, activation='tanh', input_dim=16))
+
         self.valueFunction.add(Dense(64, activation='tanh'))
+
         self.valueFunction.add(Dense(1))
         self.valueFunction.add(Activation('linear'))
 
@@ -88,6 +87,12 @@ class NumberCatchingAI:
 
     
     def getState(self):
+        """
+        Records current game state. Note: State does not include information about turns or score.
+
+        Returns:
+            The state as a list
+        """
         ret = []
         for i in range(len(self.numberInfo)):
             ret.append(self.numberInfo[i].value)
@@ -97,6 +102,12 @@ class NumberCatchingAI:
         return ret
 
     def getBestAction(self):
+        """
+        Chooses best action based on weighted values from softmax outputs of the policy function
+
+        Returns:
+            The action as in integer (-1 for left, 0 for stay, 1 for right)
+        """
         
         state = self.getState()
 
@@ -115,6 +126,9 @@ class NumberCatchingAI:
     
 
     def drawGame(self):
+        """
+        Draws the game so that user can either play or watch the game.
+        """
 
         mat = [[0 for x in range(self.numCols)] for y in range(self.numRows)]
 
@@ -137,11 +151,16 @@ class NumberCatchingAI:
         
         print("Score: " + str(self.score))
         print("Turn " + str(self.currentTurn) + "/" + str(self.maxTurns))
-        print("Best action = " + str(self.getBestAction()))
 
 
 
     def setState(self, state):
+        """
+        Sets the game to a specified state. Note: State does not include information about turn number or score
+
+        Args:
+            state: the state to set to
+        """
         # number of numbers in the queue
         self.numberInfo.clear()
         
@@ -154,6 +173,17 @@ class NumberCatchingAI:
 
 
     def rewardFunction(self, state, action):
+        """
+        Measures reward given by being in a state and taking an action.
+
+        Args:
+            state: The state prior to the action being taken
+
+            action: The action that is taken to observe reward
+
+        Returns:
+            The reward
+        """
 
         #keep old data here for later use
         oldState = self.getState()
@@ -198,6 +228,9 @@ class NumberCatchingAI:
             
 
     def playGameHuman(self):
+        """
+        Prints out game to the terminal so you can play!
+        """
 
         while( self.currentTurn <= self.maxTurns):
             #draw game
@@ -221,8 +254,12 @@ class NumberCatchingAI:
 
 
     def performAction(self, action):
+        """
+        Performs action and progresses game.
 
-        #self.score += self.rewardFunction(self.getState(), action)
+        Args:
+            action: The action that will be applied to the game
+        """
         #move player
         if(self.playerLocation == 14 and action == 1):
             self.playerLocation = 14
@@ -246,6 +283,12 @@ class NumberCatchingAI:
 
 
     def runGameAI(self):
+        """
+        Measures agent performance on one game
+
+        Returns:
+            The agent's score
+        """
         self.score = 0
         for i in range(200):
             action = self.getBestAction()
@@ -256,6 +299,15 @@ class NumberCatchingAI:
         return self.score
     
     def runGameAISamples(self, numSamples):
+        """
+        Measures performance of agent by averaging scores of independent games.
+
+        Args:
+            numSamples: The number of samples to collect. Small numbers calculate fast, but with poor estimation of the mean.
+
+        Returns:
+            Average score of the games.
+        """
         scores = []
         for i in range(numSamples):
             scores.append(self.runGameAI())
@@ -263,6 +315,17 @@ class NumberCatchingAI:
         return np.mean(scores)
 
     def cumulativeReward(self, timestep, rewards):
+        """
+        Calculates the cumulative discounted reward. Used in training the value function
+
+        Args:
+            timestep: The timestep to calculate rewards from.
+
+            rewards: A list of rewards collected.
+
+        Returns:
+            Returns the cumulative discounted reward
+        """
         ret = 0
 
         for t in range(timestep, min(len(rewards), len(rewards) + self.horizon)):
@@ -272,9 +335,29 @@ class NumberCatchingAI:
 
 
     def getValue(self, state):
+        """
+        Uses value function to predict estimated rewards from a state
+
+        Args:
+            state: The state input
+
+        Returns:
+            Returns the estimated rewards from the input state
+        """
         return (self.valueFunction.predict(np.array([state])))[0][0]
 
     def calculateGAEDeltas(self, rewards, states):
+        """
+        Calculates the GAEDeltas used in calculating advantages. Equation from the PPO paper
+
+        Args:
+            rewards: A list of rewards (each being a scalar)
+            
+            states: A list of states (each being a list)
+
+        Returns:
+            Returns a list of the GAE deltas to be used to calculate advantages
+        """
         ret = []
 
         for t in range(len(rewards) - 1):
@@ -284,6 +367,17 @@ class NumberCatchingAI:
         return ret
 
     def calculateAdvantage(self, timestep, GAEDeltas):
+        """
+        Calculates advantage using the Generalized Advantage estimation found in the PPO paper
+
+        Args:
+            timestep: The timestep to calculate from
+
+            GAEDeltas: A list of precomputed GAEDeltas
+
+        Returns:
+            The advantage calculated
+        """
         r = 0
         for t in range(timestep, len(GAEDeltas)):
             r += (self.GAELambda * self.discountFactor)**(t - timestep) * GAEDeltas[t]
@@ -292,6 +386,19 @@ class NumberCatchingAI:
 
 
     def calculateSoftmaxTarget(self, oldSoftmax, action, advantage):
+        """
+        Calculates the new softmax output based on whether or not an advantage is positive or negative.
+
+        Args:
+            oldSoftmax: The current softmax values.
+            
+            action: The action that was taken as an integer
+
+            advantage: Scalar value representing how well that action did
+
+        Returns:
+            New Softmax output that only deviates from old softmax by the model's epsilon hyperparameter
+        """
         ret = [0,0,0]
 
         probNonActions = 0
@@ -325,10 +432,32 @@ class NumberCatchingAI:
 
 
     def getProbability(self, state, action):
+        """
+        Returns the probability of the action given the state using the policy function
+
+        Args:
+            state: A list representing the state
+
+            action: The action that is taken as an integer
+
+        Returns:
+            Probability P( action | state )
+        """
         return self.policyFunction.predict(np.array([state]))[0][action + 1]
 
 
     def getAvgProbability(self, states, actions):
+        """
+        Computes the average probability of actions in a recorded trajectory. Good metric to see how "confident" the policy is
+
+        Args:
+            states: a list of the states in the trajectory
+
+            actions: a list of the actions in the trajectory.
+
+        Returns:
+            The average probability
+        """
         r = 0
 
         for i in range(len(states)):
@@ -336,7 +465,18 @@ class NumberCatchingAI:
 
         return r / len(states)
 
-    def getRunningAverage(self, data, n):
+    def getFloatingAverage(self, data, n):
+        """
+        Returns the floating average of a sequence of numbers
+
+        Args:
+            data: a list of numbers (scalars)
+            
+            n: The period to average over
+
+        Returns:
+            The floating average.
+        """
         if n >= len(data):
             r = 0
 
@@ -353,6 +493,15 @@ class NumberCatchingAI:
 
 
     def getAvgAbsAdvantage(self, advantages):
+        """
+        Debugging function to see how the absolute value of the advantages changes over time
+
+        Args:
+            advantages: a list of advantanges (scalar)
+
+        Returns:
+            Average absolute value of the advantages
+        """
         r = 0
 
         for i in range(len(advantages)):
@@ -361,6 +510,12 @@ class NumberCatchingAI:
         return r / len(advantages)
 
     def playGameAI(self, policyName):
+        """
+        Agent plays game by using policy parameters
+
+        Args:
+            policyName: The name of the policy to load. Should be in the same folder as this file
+        """
 
         self.policyFunction = keras.models.load_model(policyName)
 
@@ -375,6 +530,15 @@ class NumberCatchingAI:
             
 
     def trainAIPPO(self, iterations, timesteps):
+        """
+        Trains the agent using the Proximal Policy Optimization algorithm
+
+        Args:
+            iterations: Number of training iterations to perform
+            
+            timesteps: Number of timesteps to collect data per training iteration
+
+        """
         scores = []
         maxScore = -250
 
@@ -457,8 +621,8 @@ p - play the game as a human
 w - watch the AI play with the policy "BestPolicy.h5" in the folder
 """
 
-if len(sys.argv) < 2:
-    print("Need a command line arg. Options are t (train), p (play), w (watch)")
+if len(sys.argv) is not 2:
+    print("Need 1 command line arg. Options are t (train), p (play), w (watch)")
     exit()
 
 n = NumberCatchingAI(200)
@@ -466,8 +630,10 @@ n = NumberCatchingAI(200)
 if sys.argv[1] == 't':
     n.trainAIPPO(trainingIterations, timestepsPerIteration)
 elif sys.argv[1] == 'p':
+    print("Press a,s,d then enter to move bucket.")
     n.playGameHuman()
 elif sys.argv[1] == 'w':
+    print("Press enter to advance the game.")
     n.playGameAI("BestPolicy.h5")
 else:
     print("Bad command line arg. Options are t (train), p (play), w (watch)")
